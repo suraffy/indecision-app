e981442d5fe38119d34f9ad50bd2802c90e043c3
Surafel <surafelay@gmail.com>
2023-10-07 20:16:40 +0300

    convert class component to sfc and display errors

--- src/components/AddOption.jsx
+++ src/components/AddOption.jsx
@@ -1,21 +1,32 @@
-const AddOption = (props) => {
-  const handleSubmit = (e) => {
+import React, { Component } from "react";
+
+class AddOption extends Component {
+  state = { error: undefined };
+
+  handleSubmit = (e) => {
     e.preventDefault();
 
     const optionEl = e.target.elements.option;
+    const text = optionEl.value.trim();
 
-    const error = props.onAddOption(optionEl.value.trim());
+    const error = this.props.onAddOption(text);
+
+    this.setState(() => ({ error: error }));
 
     if (!error) optionEl.value = "";
-    console.log(error);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="option" />
-      <button>Add Option</button>
-    </form>
-  );
-};
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddOption;
